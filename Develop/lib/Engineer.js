1e633e5264d307ed543326a393f6872c7e3577df
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-10-24 11:52:24 -0700

    it is working

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,42 +1,9 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-const ansEngineer = [
-    { 
-        type: 'input',
-        message: 'What is the Engineer name?',
-        name: 'name'
-    },
-    {
-        type: 'input',
-        message: 'What is ID of this user?',
-        name: 'id'
-    },
-    {
-        type: 'input',
-        message: 'What is the Email of this user?',
-        name: 'email',
-        validate: function (email) {
-            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-            if (valid) {
-                console.log(" confirmed");
-                return true;
-            } else {
-                console.log(" please Enter Valid email");
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        message: 'What is the GitHub name of this user?',
-        name: 'github'
-    }
-]
+const Employee = require("./Employee");
 
-class Engineer {
+class Engineer extends Employee {
     constructor (name, id, email, github) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
+        super(name, id, email)
         this.github = github;
         this.role = 'Engineer'
     }
@@ -45,25 +12,10 @@ class Engineer {
         return 'Engineer';
     }
 
-    getName() {
-        return this.name;
-    }
-
-    getId() {
-        return this.id;
-    }
-
-    getEmail() {
-        return this.email;
-    }
-
     getGithub() {
         return this.github;
     }
 
 }
 
-module.exports = {
-    ansEngineer,
-    Engineer
-}
+module.exports = Engineer;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,44 +1,9 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
-const inquirer = require("inquirer");
+const Employee = require("./Employee");
 
-const ansManager = [
-    { 
-        type: 'input',
-        message: 'What is the UserName(Manager Name)?',
-        name: 'name'
-    },
-    {
-        type: 'input',
-        message: 'What is ID of this user?',
-        name: 'id'
-    },
-    {
-        type: 'input',
-        message: 'What is the Email of this user?',
-        name: 'email',
-        validate: function (email) {
-            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-            if (valid) {
-                console.log(" confirmed");
-                return true;
-            } else {
-                console.log(" please Enter Valid email");
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        message: 'What is the Office Number?',
-        name: 'officeNumber'
-    }
-]
-
-class Manager {
+class Manager extends Employee {
     constructor(name, id, email, officeNumber) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
+        super(name, id, email)
         this.officeNumber = officeNumber;
         this.role = 'Manager'
     }
@@ -47,30 +12,9 @@ class Manager {
         return 'Manager';
     }
 
-    getName() {
-        return this.name;
-    }
-
-    getId() {
-        return this.id;
-    }
-
-    getEmail() {
-        return this.email;
-    }
-
     getOfficeNumber() {
         return this.officeNumber;
     }
 }
 
-
-// async function getManager () {
-//     const ans = await inquirer.prompt(ansManager)
-//     return ans;
-// }
-
-module.exports = {
-    Manager,
-    ansManager
-}
+module.exports = Manager;
